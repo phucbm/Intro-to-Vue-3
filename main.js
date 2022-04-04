ec26c92e62eab66c7f2138e88361c28868f9b5fe
phucbm <hiphucbui@gmail.com>
2022-04-04 21:24:30 +0700

    attribute binding

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
     data(){
-        return {product: 'Socks', description: 'Lorem ipsum'}
+        return {
+            product: 'Socks',
+            description: 'Lorem ipsum',
+            image: './assets/images/socks_blue.jpg',
+            url: '#'
+        }
     }
 }).mount('#app');
