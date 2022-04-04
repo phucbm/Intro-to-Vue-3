8d4709d97a6e1ac065ff80eaa64b62ed7084a9c8
phucbm <hiphucbui@gmail.com>
2022-04-04 21:46:06 +0700

    event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data(){
         return {
+            cart: 0,
             product: 'Socks',
             description: 'Lorem ipsum',
             image: './assets/images/socks_blue.jpg',
@@ -9,9 +10,14 @@ const app = Vue.createApp({
             inventory: 15,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 123, color: 'green'},
-                {id: 234, color: 'blue'}
+                {id: 123, color: 'green', image: './assets/images/socks_green.jpg',},
+                {id: 234, color: 'blue', image: './assets/images/socks_blue.jpg',}
             ]
+        }
+    },
+    methods: {
+        removeFromCart(){
+            this.cart--;
         }
     }
 }).mount('#app');
